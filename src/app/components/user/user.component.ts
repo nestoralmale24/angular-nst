9e05c170deb7f56a940ca605c2b2e090582df050
nestoralmale24 <nestordic2004@gmail.com>
2024-03-06 21:33:54 +0100

    show user component and close

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'user-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+username:string = "Nst"
 }
